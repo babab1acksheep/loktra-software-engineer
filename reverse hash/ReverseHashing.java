804d315acdf476d5cdff321d782191a37e42649a
Aravinth Chandramouli <aravinth.chandramouli@gmail.com>
2016-05-24 15:25:13 +0530

    Made the code generic to accept input values

--- reverse hash/ReverseHashing.java
+++ reverse hash/ReverseHashing.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 
 public class ReverseHashing {
 
 	public static void main(String[] args) {
-		System.out.println(reverseHash(930846109532517L,new StringBuffer(),9));
+		Scanner inputScanner = new Scanner(System.in);
+		System.out.println("Enter the hash value:");
+		long hash = inputScanner.nextLong();
+		System.out.println("Enter the number of letters in the string:");
+		int strLength = inputScanner.nextInt();
+		System.out.println(reverseHash(hash,new StringBuffer(),strLength));
+		
 	}
 	
-	public static StringBuffer reverseHash(long finalHash, StringBuffer hashedString, int strLength){	
+	public static StringBuffer reverseHash(long finalHash, StringBuffer hashedString, int strLength){
 		String letters = "acdegilmnoprstuw";
 		if(strLength==0){
-			return hashedString.reverse();
-		}
+			if(finalHash==7)
+				return hashedString.reverse();
+			else
+				return new StringBuffer("Sorry! There is no string for the given hash with the given number of letters!");		
+		}		
 		else{
 			for(int i = 0 ; i<letters.length() ; i++){
 				if((finalHash-i)%37==0){
